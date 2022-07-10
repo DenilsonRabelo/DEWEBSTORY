0d42ef4813e9063afa2dd74976b9c4819c7221e6
Denilson <denilson2020@alu.ufc.br>
2022-07-09 23:28:23 -0300

    fiz bug from authentucated

--- src/main.js
+++ src/main.js
@@ -8,9 +8,11 @@ router.beforeEach((to,from,next) => {
     console.log(localStorage.getItem("role"))
     if(localStorage.getItem("role") == '"admin"'){
         next()
-    }else if ((to.name == 'Painel' || to.name == 'Criar' || to.path == '/editar' || to.name === 'Editar') && localStorage.getItem("role") == '"public"' || localStorage.getItem("role") == '"authenticated"'){
+    }else if ((to.name == 'Painel' || to.name == 'Criar' || to.path == '/editar' || to.name === 'Editar') && localStorage.getItem("role") == '"public"'){
         next('/home')
     }else if (!localStorage.getItem("role") && (to.name == 'Painel' || to.name == 'Criar' || to.path == '/editar' || to.name === 'Editar')){
+        next('/home')
+    }else if (localStorage.getItem("role") == '"authenticated"' && (to.name == 'Painel' || to.name == 'Criar' || to.path == '/editar' || to.name === 'Editar')){
         next('/home')
     }else {
         next()
